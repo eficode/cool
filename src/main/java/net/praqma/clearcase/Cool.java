b65e73216e26a622a2a0ecbd0448dd71ccdfa65b
unknown <cwolfgang@.cclab.praqma.net>
2006-07-25 22:58:50 +0200

    Work

--- src/main/java/net/praqma/clearcase/Cool.java
+++ src/main/java/net/praqma/clearcase/Cool.java
@@ -5,9 +5,9 @@ import net.praqma.util.execute.CommandLineInterface.OperatingSystem;
 
 public abstract class Cool {
 
-	protected static final String filesep = System.getProperty( "file.separator" );
-	protected static final String qfs = filesep.equals( "\\" ) ? "\\\\" : filesep;
-	protected static final String linesep = System.getProperty( "line.separator" );
+	public static final String filesep = System.getProperty( "file.separator" );
+	public static final String qfs = filesep.equals( "\\" ) ? "\\\\" : filesep;
+	public static final String linesep = System.getProperty( "line.separator" );
 	public static final String delim = "::";
 
 	public static OperatingSystem getOS() {
--- src/main/java/net/praqma/clearcase/test/CoolTestCase.java
+++ src/main/java/net/praqma/clearcase/test/CoolTestCase.java
@@ -2,39 +2,71 @@ package net.praqma.clearcase.test;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.BeforeClass;
 
+import net.praqma.clearcase.Cool;
 import net.praqma.clearcase.PVob;
 import net.praqma.clearcase.annotations.TestConfiguration;
+import net.praqma.clearcase.exceptions.ClearCaseException;
 import net.praqma.clearcase.exceptions.CleartoolException;
+import net.praqma.clearcase.exceptions.UCMEntityNotFoundException;
+import net.praqma.clearcase.exceptions.UnableToCreateEntityException;
+import net.praqma.clearcase.exceptions.UnableToListProjectsException;
+import net.praqma.clearcase.exceptions.UnableToLoadEntityException;
+import net.praqma.clearcase.ucm.entities.Component;
+import net.praqma.clearcase.ucm.entities.Project;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.debug.Logger.LogLevel;
+import net.praqma.util.debug.appenders.Appender;
+import net.praqma.util.debug.appenders.ConsoleAppender;
 
 import junit.framework.TestCase;
 
 public abstract class CoolTestCase extends TestCase {
 	
+	protected static Logger logger = Logger.getLogger();
+	protected static ConsoleAppender appender = new ConsoleAppender();
+	
 	protected static boolean rolling = true;
 	
-	protected PVob pvob;
-	protected boolean well = true;
-	
-	@BeforeClass
-	public static void clearcaseSetup() {
-		
+	static {
+		System.out.println( "STATIC" );
+		appender.setTemplate( "[%level]%space %message%newline" );
+		appender.setMinimumLevel(LogLevel.DEBUG);
+		Logger.addAppender(appender);
 	}
 	
+	protected PVob pvob;
+	protected boolean removePvob = false;
+	
+	protected void createComponents( int rooted, int rootless ) {
+		
+	}
+
 	@Override
 	protected void setUp() {
-		System.out.println( "Setup" );
+		logger.debug( "Setup" );
+		
 		TestConfiguration config = getClass().getAnnotation( TestConfiguration.class );
 		String project = config.project();
-		String pvob = config.pvob();
+		String pvob = Cool.filesep + config.pvob();
 		
-		try {
-			this.pvob = (PVob) PVob.create( pvob, true, null, "testing" );
-		} catch( CleartoolException e ) {
-			e.print( System.err );
-			well = false;
+		removePvob = false;
+		PVob pv = PVob.get( pvob );
+		if( pv == null ) {
+			logger.info(pvob + " does not exist, trying to create it");
+			try {
+				this.pvob = (PVob) PVob.create( pvob, true, null, "testing" );
+				removePvob = true;
+			} catch( CleartoolException e ) {
+				e.print( System.err );
+				
+			}
+		} else {
+			logger.fatal("The PVob " + pvob + " already exists" );
 		}
 		
 		System.out.println( "Project: " + project );
@@ -64,10 +96,21 @@ public abstract class CoolTestCase extends TestCase {
     protected void tearDown() {
     	System.out.println( "DOWN!" );
     	
-    	try {
-			pvob.remove();
-		} catch( CleartoolException e ) {
-			e.print( System.err );
-		}
+    	if( removePvob ) {
+    		logger.info("Getting projects");
+    		try {
+				Project.getProjects(pvob);
+			} catch (ClearCaseException e1) {
+				e1.print(appender.getOut());
+				return;
+			}
+    		
+    		logger.info("Removing PVob " + pvob);
+	    	try {
+				pvob.remove();
+			} catch( CleartoolException e ) {
+				e.print( System.err );
+			}
+    	}
     }
 }
