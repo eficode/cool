34322702de2e4b9289ab2c8375410060865808c9
wolfgang <chw@praqma.net>
2010-11-26 10:39:53 +0100

    Added utils.IO

---
+++ src/main/java/net/praqma/utils/IO.java
@@ -0,0 +1,21 @@
+package net.praqma.utils;
+
+import java.io.File;
+
+public class IO
+{
+	public static boolean DeleteDirectory( File directory )
+	{
+		if ( directory.isDirectory() )
+		{
+			String[] elements = directory.list();
+			for( int i = 0 ;  i < elements.length ; i++ ) 
+			{
+				boolean success = DeleteDirectory( new File( directory, elements[i] ) ); 
+				if ( !success ) return false;
+			} 
+		} 
+		
+		return directory.delete();
+	}
+}
