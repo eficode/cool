b2d35b251963597626139455a64dab885880fdae
wolfgang <chw@praqma.net>
2010-11-29 15:27:45 +0100

    Minor update

--- src/main/java/net/praqma/utils/Command.java
+++ src/main/java/net/praqma/utils/Command.java
@@ -45,7 +45,7 @@ public abstract class Command
 			if ( exitValue != 0 )
 			{
 				logger.log( "Abnormal process termination: " + output.sres.toString() );
-				throw new AbnormalProcessTerminationException();
+				throw new AbnormalProcessTerminationException( "Abnormal process termination: " + output.sres.toString() );
 			}
 			
 			p.getErrorStream().close();
