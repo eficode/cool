80b8091e1e31c9901e307441848c004a1b23fb3d
wolfgang <chw@praqma.net>
2010-12-14 09:24:53 +0100

    Fixed cleartool error

--- src/main/java/net/praqma/clearcase/cleartool/Cleartool.java
+++ src/main/java/net/praqma/clearcase/cleartool/Cleartool.java
@@ -21,7 +21,7 @@ public abstract class Cleartool
 {	
 	public static CmdResult run( String cmd ) throws CommandLineException, AbnormalProcessTerminationException
 	{
-		return Command.run( cmd, null, false );
+		return Command.run( "cleartool " + cmd, null, false );
 	}
 	
 	public static CmdResult run( String cmd, File dir ) throws CommandLineException, AbnormalProcessTerminationException
