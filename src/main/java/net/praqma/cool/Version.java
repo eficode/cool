a31e94680605dd69cc0fe82614c001bef283022f
wolfgang <chw@praqma.net>
2011-01-25 10:56:48 +0100

    Added Version.java

---
+++ src/main/java/net/praqma/cool/Version.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package net.praqma.cool;
+
+/**
+ * @author jssu
+ * 
+ */
+public class Version
+{
+	private static final String major    = "0";
+	private static final String minor    = "2";
+	private static final String patch    = "1";
+	private static final String sequence = "XXXX";
+	
+	public static final  String version  = major + '.' + minor + '.' + patch + '.' + sequence;
+}
