61e355ff939c1723ddcb01f8f3ba70e30f3eedd6
wolfgang <chw@praqma.net>
2011-02-03 14:54:40 +0100

    Enhanced UCMException

--- src/main/java/net/praqma/clearcase/ucm/UCMException.java
+++ src/main/java/net/praqma/clearcase/ucm/UCMException.java
@@ -1,9 +1,16 @@
 package net.praqma.clearcase.ucm;
 
+import net.praqma.util.debug.Logger;
+
 //public class UCMException extends RuntimeException
 public class UCMException extends Exception
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5325867242379727760L;
 	public UCMType type = UCMType.DEFAULT;
+	private Logger logger = Logger.getLogger();
 	
 	public enum UCMType
 	{
@@ -12,23 +19,29 @@ public class UCMException extends Exception
 		ENTITY_NAME_ERROR,
 		LOAD_FAILED,
 		TAG_CREATION_FAILED,
-		UNKNOWN_TAG,
+		UNKNOWN_HLINK_TYPE,
 		VIEW_ERROR
 	}
 	
 	public UCMException()
 	{
-		super(); 
+		super();
+		
+		logger.warning( "Unnamed UCMException thrown" );
 	}
 	
 	public UCMException( String s )
 	{
-		super( s ); 
+		super( s );
+		
+		logger.warning( s );
 	}
 	
 	public UCMException( String s, UCMType type )
 	{
 		super( s );
+		
+		logger.warning( s );
 		
 		this.type = type;
 	}
