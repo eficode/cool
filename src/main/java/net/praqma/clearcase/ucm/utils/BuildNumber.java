1d4a63c109a3a2188ba51e7b895be2251051b5b3
wolfgang <chw@praqma.net>
2011-02-03 14:55:47 +0100

    Adding BuildNumber utility for cc

--- src/main/java/net/praqma/clearcase/ucm/utils/BuildNumber.java
+++ src/main/java/net/praqma/clearcase/ucm/utils/BuildNumber.java
@@ -1,42 +1,187 @@
 package net.praqma.clearcase.ucm.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.praqma.clearcase.ucm.UCMException;
 import net.praqma.clearcase.ucm.entities.Baseline;
 import net.praqma.clearcase.ucm.entities.Component;
+import net.praqma.clearcase.ucm.entities.HyperLink;
+import net.praqma.clearcase.ucm.entities.Project;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.io.BuildNumberStamper;
+import net.praqma.util.structure.Tuple;
 
 public class BuildNumber
 {
 	private Baseline baseline = null;
-	private final Pattern pattern_parse_baseline = Pattern.compile( "^\\S+__(\\d+)_(\\d+)_(\\d+)_(\\d+)$" );
+	private final static Pattern pattern_parse_baseline = Pattern.compile( "^\\S+__(\\d+)_(\\d+)_(\\d+)_(\\d+)$" );
+	
+	private static Logger logger = Logger.getLogger();
 	
 	private Integer major    = null;
 	private Integer minor    = null;
 	private Integer patch    = null;
 	private Integer sequence = null;
 	
-	public BuildNumber( Baseline baseline ) throws UCMException
+	public static final String __BUILD_NUMBER_MAJOR    = "buildnumber.major";
+	public static final String __BUILD_NUMBER_MINOR    = "buildnumber.minor";
+	public static final String __BUILD_NUMBER_PATCH    = "buildnumber.patch";
+	public static final String __BUILD_NUMBER_SEQUENCE = "buildnumber.sequence";
+	public static final String __BUILD_NUMBER_FILE     = "buildnumber.file";
+	
+	public static Tuple<Baseline, String[]> createBuildNumber( String baseline, Component component, File view ) throws UCMException
 	{
-		this.baseline = baseline;
-		Matcher match = pattern_parse_baseline.matcher( baseline.GetShortname() );
+		Matcher match = pattern_parse_baseline.matcher( baseline );
 		
 		if( !match.find() )
 		{
-			throw new UCMException( "The given Baseline, " + baseline.GetShortname() + ", was not recognized as a build number." );
+			throw new UCMException( "The given Baseline name, " + baseline + ", was not recognized as a build number." );
 		}
 		
+		Tuple<Baseline, String[]> result = new Tuple<Baseline, String[]>();
+		
+		Baseline bl = Baseline.create( "baseline:" + baseline, component, view, true, true );
+		
+		result.t1 = bl;
+		result.t2 = new String[4];
+		
+		result.t2[0] = match.group( 1 );
+		result.t2[1] = match.group( 2 );
+		result.t2[2] = match.group( 3 );
+		result.t2[3] = match.group( 4 );
+		
+		return result;
+		
+		/*
 		major    = Integer.parseInt( match.group( 1 ) );
 		minor    = Integer.parseInt( match.group( 2 ) );
 		patch    = Integer.parseInt( match.group( 3 ) );
-		sequence = Integer.parseInt( match.group( 4 ) );		
+		sequence = Integer.parseInt( match.group( 4 ) );
+		*/		
 	}
 	
-	public void GetNextBuildNumber() throws UCMException
+	public static void stampFromComponent( Component component, File dir, String major, String minor, String patch, String sequence ) throws UCMException
 	{
-		Component c = this.baseline.GetStream().GetSingleTopComponent();
+		List<HyperLink> result = component.getHlinks( __BUILD_NUMBER_FILE, dir );
+		
+		System.out.println( "I am here!" );
+		
+		for( HyperLink h : result )
+		{
+			String f = h.getValue().replaceFirst( "@@\\s*$", "" );
+			File stampee = new File( f );
+			BuildNumber.stampIntoCode( stampee, major, minor, patch, sequence );
+		}
 	}
+	
+	public static void stampIntoCode( File file, String major, String minor, String patch, String sequence ) throws UCMException
+	{
+		BuildNumberStamper stamp = null;
+		try
+		{
+			stamp = new BuildNumberStamper( file );
+		}
+		catch ( IOException e )
+		{
+			throw new UCMException( "Could not create temporary file" );
+		}
+		
+		try
+		{
+			stamp.stampIntoCode( major, minor, patch, sequence );
+			System.out.println( "Stamping file " + file );
+		}
+		catch ( IOException e )
+		{
+			logger.log( "Cannot access file, trying to hijack it" );
+			
+			file.setWritable( true );
+			
+			try
+			{
+				stamp.stampIntoCode( major, minor, patch, sequence );
+				System.out.println( "Stamping file " + file );
+			}
+			catch ( IOException e2 )
+			{
+				throw new UCMException( "Could not access file" );
+			}
+		}
+		
+
+	}
+	
+	/**
+	 * Given a Project, its top level Components sequence number is retrieved, incremented, stored and returned.
+	 * @param project A UCM Project
+	 * @return
+	 * @throws UCMException
+	 */
+	public static Integer getNextBuildSequence( Project project ) throws UCMException
+	{
+		Component c = project.GetStream().GetSingleTopComponent();
+		
+		/* Get the build number sequence */
+		Map<String, String> catts = c.getAttributes();
+		if( !catts.containsKey( __BUILD_NUMBER_SEQUENCE ) )
+		{
+			throw new UCMException( "The Component did not have the " + __BUILD_NUMBER_SEQUENCE + " attribute." );
+		}
+		
+		Integer sequence = Integer.parseInt( catts.get( __BUILD_NUMBER_SEQUENCE ) );
+		sequence++;
+
+		c.setAttribute( __BUILD_NUMBER_SEQUENCE, sequence.toString() );
+		
+		return sequence;
+	}
+	
+	
+	/**
+	 * This method returns the new build number for a Baseline.
+	 * @return
+	 * @throws UCMException
+	 */
+	public static String getBuildNumber( Project project ) throws UCMException
+	{
+		String exceptionMsg = "";
+		
+		/* Get build number info */
+		Map<String, String> patts = project.getAttributes();
+		if( !patts.containsKey( __BUILD_NUMBER_MAJOR ) )
+		{
+			exceptionMsg += "The Project did not have the " + __BUILD_NUMBER_MAJOR + " attribute. ";
+		}
+		if( !patts.containsKey( __BUILD_NUMBER_MINOR ) )
+		{
+			exceptionMsg += "The Project did not have the " + __BUILD_NUMBER_MINOR + " attribute. ";
+		}
+		if( !patts.containsKey( __BUILD_NUMBER_PATCH ) )
+		{
+			exceptionMsg += "The Project did not have the " + __BUILD_NUMBER_PATCH + " attribute. ";
+		}
+		
+		/* Throw if any errors */
+		if( exceptionMsg.length() > 0 )
+		{
+			throw new UCMException( exceptionMsg );
+		}
+		
+		String sequence = getNextBuildSequence( project ).toString();
+		
+		/*  If this is reached, all is good.... */
+		String major    = patts.get( __BUILD_NUMBER_MAJOR );
+		String minor    = patts.get( __BUILD_NUMBER_MINOR );
+		String patch    = patts.get( __BUILD_NUMBER_PATCH );
+				
+		return "__" + major + "_" + minor + "_" + patch + "_" + sequence;
+	}
+	
 	
 	
 }
