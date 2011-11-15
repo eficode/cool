53eb0bd93bc96fe3df72b43f914adeabc56780c0
wolfgarnet <wolfgarnet@gmail.com>
2011-11-15 13:35:27 +0100

    Added new CLI

---
+++ src/main/java/net/praqma/cli/CleanView.java
@@ -0,0 +1,81 @@
+package net.praqma.cli;
+
+import java.io.File;
+import java.util.Map;
+
+import net.praqma.clearcase.ucm.UCMException;
+import net.praqma.clearcase.ucm.entities.UCM;
+import net.praqma.clearcase.ucm.view.SnapshotView;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.debug.Logger.LogLevel;
+import net.praqma.util.debug.appenders.StreamAppender;
+import net.praqma.util.option.Option;
+import net.praqma.util.option.Options;
+
+public class CleanView {
+	
+	private static Logger logger = Logger.getLogger();
+	private static StreamAppender app = new StreamAppender( System.out );
+	
+	public static void main( String[] args ) throws UCMException {
+		try {
+			run( args );
+		} catch( UCMException e ) {
+			System.err.println( UCM.getMessagesAsString() );
+			throw e;
+		}
+	}
+
+	public static void run( String[] args ) throws UCMException {
+		Options o = new Options();
+
+		Option opath = new Option( "path", "p", false, 1, "ClearCase view to be cleaned" );
+		Option oroot = new Option( "root", "r", false, 0, "Clean root directory" );
+
+		o.setOption( opath );
+		o.setOption( oroot );
+
+        app.setTemplate( "[%level]%space %message%newline" );
+        Logger.addAppender( app );
+        
+        o.setDefaultOptions();
+        
+        o.parse( args );
+        
+        if( o.isVerbose() ) {
+        	app.setMinimumLevel( LogLevel.VERBOSE );
+        } else {
+        	app.setMinimumLevel( LogLevel.INFO );
+        }
+
+		try {
+			o.checkOptions();
+		} catch( Exception e ) {
+			logger.error( "Incorrect option: " + e.getMessage() );
+			o.display();
+			System.exit( 1 );
+		}
+
+		/* Do the ClearCase thing... */
+		UCM.setContext( UCM.ContextType.CLEARTOOL );
+		
+		File viewroot = null;
+		if( opath.isUsed() ) {
+			viewroot = new File( opath.getString() );
+		} else {
+			viewroot = new File( System.getProperty( "user.dir" ) );
+		}
+		
+		boolean exclude = true;
+		if( oroot.isUsed() ) {
+			exclude = false;
+		}
+
+		SnapshotView view = SnapshotView.getSnapshotViewFromPath( viewroot );
+		Map<String, Integer> info = view.swipe( exclude );
+		
+		logger.info( "Removed " + info.get( "files_deleted" ) + " file" + ( info.get( "files_deleted" ) == 1 ? "" : "s" ) );
+		logger.info( "Removed " + info.get( "dirs_deleted" ) + " director" + ( info.get( "dirs_deleted" ) == 1 ? "y" : "ies" ) );		
+	}
+
+}
---
+++ src/main/java/net/praqma/cli/RemoveAllViews.java
@@ -0,0 +1,67 @@
+package net.praqma.cli;
+
+import java.util.List;
+
+import net.praqma.clearcase.Region;
+import net.praqma.clearcase.Site;
+import net.praqma.clearcase.ucm.UCMException;
+import net.praqma.clearcase.ucm.entities.UCM;
+import net.praqma.clearcase.ucm.view.UCMView;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.debug.Logger.LogLevel;
+import net.praqma.util.debug.appenders.StreamAppender;
+import net.praqma.util.option.Option;
+import net.praqma.util.option.Options;
+
+public class RemoveAllViews {
+	private static Logger logger = Logger.getLogger();
+	private static StreamAppender app = new StreamAppender( System.out );
+	
+	public static void main( String[] args ) {
+		
+        Options o = new Options( "1.0.0" );
+
+        Option oregion = new Option( "region", "r", true, 1, "Name of the region" );
+        
+        o.setOption( oregion );
+        
+        o.setDefaultOptions();
+        
+        o.parse( args );
+        
+        app.setTemplate( "[%level]%space %message%newline" );
+        Logger.addAppender( app );
+                
+        if( o.isVerbose() ) {
+        	app.setMinimumLevel( LogLevel.DEBUG );
+        } else {
+        	app.setMinimumLevel( LogLevel.INFO );
+        }
+        
+
+        try {
+            o.checkOptions();
+        } catch( Exception e ) {
+        	logger.error( "Incorrect option: " + e.getMessage() );
+            o.display();
+            System.exit( 1 );
+        }
+
+		/* Do the ClearCase thing... */
+		UCM.setContext( UCM.ContextType.CLEARTOOL );
+		
+		Site site = new Site( "My site" );
+		Region region = new Region( oregion.getString(), site );
+		
+		List<UCMView> views = region.getViews();
+		
+		for( UCMView view : views ) {
+			try {
+				logger.info( "Removing " + view.getViewtag() );
+				view.removeView();
+			} catch (UCMException e) {
+				logger.warning( "Failed to remove " + view.getViewtag() );
+			}
+		}
+	}
+}
