f3e0c59317d44f62fec60e05186631052f5545ad
wolfgarnet <wolfgarnet@gmail.com>
2012-03-23 10:34:48 +0100

    Adding cool jenkins test

---
+++ clearcase-jenkins-test/src/main/java/net/praqma/jenkins/utils/test/ClearCaseJenkinsTestCase.java
@@ -0,0 +1,33 @@
+package net.praqma.jenkins.utils.test;
+
+import net.praqma.clearcase.test.junit.CoolTestCase;
+
+import org.jvnet.hudson.test.HudsonTestCase;
+
+public class ClearCaseJenkinsTestCase extends HudsonTestCase {
+	public CoolTestCase coolTest = new CoolTestCase();
+	
+	public CoolTestCase getCoolTestCase() {
+		return coolTest;
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		coolTest.setUp();
+		super.setUp();
+	}
+	
+	@Override
+	public void runTest() throws Throwable {
+		coolTest.runTest();
+		if( !coolTest.hasFailed() ) {
+			super.runTest();
+		}
+	}
+	
+	@Override
+	public void tearDown() throws Exception {
+		coolTest.tearDown();
+		super.tearDown();
+	}
+}
