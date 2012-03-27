f79428304e77beb40c880f79f0d20b16b42ab690
Mads Nielsen <inkspot84@gmail.com>
2012-03-27 21:02:49 +0200

    Merged

--- core/src/main/java/net/praqma/clearcase/ucm/entities/Project.java
+++ core/src/main/java/net/praqma/clearcase/ucm/entities/Project.java
@@ -2,6 +2,7 @@ package net.praqma.clearcase.ucm.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import net.praqma.clearcase.PVob;
 import net.praqma.clearcase.cleartool.Cleartool;
