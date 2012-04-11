e3d6afb13e304bebe41da9b87ae5c5460a78a481
wolfgarnet <wolfgarnet@gmail.com>
2012-04-11 13:25:31 +0200

    Enhanced main

--- core/src/main/java/net/praqma/cli/Main.java
+++ core/src/main/java/net/praqma/cli/Main.java
@@ -1,12 +1,31 @@
 package net.praqma.cli;
 
+import java.util.List;
+
+import net.praqma.util.PackageUtils;
+
 public class Main {
 	public static void main( String[] args ) throws Exception {
-		/* Fist argument is sub program */
-		String app = args[0];
 		
-		Class<CLI> clazz = (Class<CLI>) Class.forName( "net.praqma.cli." + app );
-		CLI cli = clazz.newInstance();
-		cli.perform( args );
+		if( args.length > 0 ) {
+			/* Fist argument is sub program */
+			String app = args[0];
+			
+			Class<CLI> clazz = (Class<CLI>) Class.forName( "net.praqma.cli." + app );
+			CLI cli = clazz.newInstance();
+			cli.perform( args );
+		} else {
+			List<Class<?>> list = PackageUtils.getClasses( "net.praqma.cli", null );
+			
+			System.out.println( "Possible sub programs:" );
+			for( Class<?> c : list ) {
+				if( CLI.class.isAssignableFrom( c ) && !c.equals( CLI.class ) ) {
+					Class<? extends CLI> c1 = (Class<? extends CLI>) c;
+					System.out.println( " * " + c.getSimpleName() );
+				}
+			}
+			
+			
+		}
 	}
 }
