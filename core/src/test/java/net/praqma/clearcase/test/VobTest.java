75447fa811a8ae4642e6ec033350a07c3f3aa586
wolfgarnet <wolfgarnet@gmail.com>
2012-04-11 13:39:07 +0200

    Added vob tests

---
+++ core/src/test/java/net/praqma/clearcase/test/VobTest.java
@@ -0,0 +1,47 @@
+package net.praqma.clearcase.test;
+
+import net.praqma.clearcase.PVob;
+import net.praqma.clearcase.Vob;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class VobTest extends TestCase {
+
+	@Test
+	public void testVobEqualsDifferent() {
+		Vob v1 = new Vob( "/vob1" );
+		Vob v2 = new Vob( "/vob2" );
+		
+		assertNotSame( v1, v2 );
+		assertFalse( v1.hashCode() == v2.hashCode() );
+	}
+	
+	@Test
+	public void testVobEquals() {
+		Vob v1 = new Vob( "/vob1" );
+		Vob v2 = new Vob( "/vob1" );
+		
+		assertEquals( v1, v2 );
+		assertTrue( v1.hashCode() == v2.hashCode() );
+	}
+	
+	@Test
+	public void testPVobEqualsDifferent() {
+		PVob v1 = new PVob( "/vob_pvob1" );
+		PVob v2 = new PVob( "/vob_pvob2" );
+		
+		assertNotSame( v1, v2 );
+		assertFalse( v1.hashCode() == v2.hashCode() );
+	}
+	
+	@Test
+	public void testPVobEquals() {
+		PVob v1 = new PVob( "/vob_pvob1" );
+		PVob v2 = new PVob( "/vob_pvob1" );
+		
+		assertEquals( v1, v2 );
+		assertTrue( v1.hashCode() == v2.hashCode() );
+	}
+}
