a9901fdeb23f8fa75c3a5bc9abb190a9be1aad9b
wolfgarnet <wolfgarnet@gmail.com>
2012-04-12 21:15:15 +0200

    Adding checkin task

---
+++ core/src/main/java/net/praqma/clearcase/util/setup/CheckinTask.java
@@ -0,0 +1,21 @@
+package net.praqma.clearcase.util.setup;
+
+import java.io.File;
+
+import net.praqma.clearcase.exceptions.ClearCaseException;
+import net.praqma.clearcase.ucm.entities.Version;
+import net.praqma.clearcase.util.setup.EnvironmentParser.Context;
+
+import org.w3c.dom.Element;
+
+public class CheckinTask extends AbstractTask {
+
+	@Override
+	public void parse( Element e, Context context ) throws ClearCaseException {
+		File file = new File( e.getAttribute( "file" ) );
+		String comment = e.getAttribute( "comment" ).length() > 0 ? e.getAttribute( "comment" ) : null;
+		
+		Version.checkIn( file, false, context.path, comment );
+	}
+
+}
