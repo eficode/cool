e6062b35faaae4ecd55fb172a9b89e22871661b6
wolfgarnet <wolfgarnet@gmail.com>
2012-05-02 15:31:40 +0200

    Added unit tests and powermock

---
+++ core/src/test/java/net/praqma/clearcase/test/unit/StreamUnitTest.java
@@ -0,0 +1,51 @@
+package net.praqma.clearcase.test.unit;
+
+import net.praqma.clearcase.cleartool.Cleartool;
+import net.praqma.clearcase.exceptions.UnableToInitializeEntityException;
+import net.praqma.clearcase.ucm.entities.Baseline;
+import net.praqma.clearcase.ucm.entities.Stream;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.debug.Logger.LogLevel;
+import net.praqma.util.debug.appenders.Appender;
+import net.praqma.util.debug.appenders.ConsoleAppender;
+import net.praqma.util.execute.AbnormalProcessTerminationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import junit.framework.TestCase;
+
+@RunWith( PowerMockRunner.class )
+@PrepareForTest( { Baseline.class, Cleartool.class } )
+public class StreamUnitTest extends TestCase {
+	
+	static {
+		Appender appender = new ConsoleAppender();
+		appender.setMinimumLevel( LogLevel.DEBUG );
+		Logger.addAppender( appender );
+	}
+	
+	@Test
+	public void testStreamExists() throws UnableToInitializeEntityException {
+		Stream stream = Stream.get( "stream@\\pvob" );
+		
+		PowerMockito.mockStatic( Cleartool.class );
+		PowerMockito.when( Cleartool.run( Mockito.any( String.class ) ) ).thenReturn( null );
+		
+		assertTrue( stream.exists() );
+	}
+	
+	@Test
+	public void testStreamNotExists() throws UnableToInitializeEntityException {
+		Stream stream = Stream.get( "stream@\\pvob" );
+		
+		PowerMockito.mockStatic( Cleartool.class );
+		PowerMockito.when( Cleartool.run( Mockito.any( String.class ) ) ).thenThrow( new AbnormalProcessTerminationException( "DOESNT", "EXIST" ) );
+		
+		assertFalse( stream.exists() );
+	}
+}
