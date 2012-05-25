5e849a3258b7238e353361ff59cd6bb67ff04fdb
wolfgarnet <wolfgarnet@gmail.com>
2012-05-25 22:45:19 +0200

    Getting path

--- core/src/test/java/net/praqma/clearcase/test/ComponentTest.java
+++ core/src/test/java/net/praqma/clearcase/test/ComponentTest.java
@@ -61,7 +61,7 @@ public class ComponentTest extends CoolTestCase {
 	public void testGetRootDir() throws Exception {
 		Component model = context.components.get( "Model" );
 		
-		String root = new File( "/" + uniqueTestVobName + "/" + "Model" ).getName();
+		String root = new File( "/" + uniqueTestVobName + "/" + "Model" ).getPath();
 		System.out.println( "---->ROOT: " + uniqueTestVobName );
 		System.out.println( "---->ROOT: " + root );
 		assertEquals( root, model.getRootDir() );
