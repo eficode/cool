981a58e03dd72c0cc8d116ac5f78f62ee394f09b
wolfgarnet <wolfgarnet@gmail.com>
2012-05-25 16:13:02 +0200

    Printing more debug

--- core/src/main/java/net/praqma/clearcase/util/setup/BaselineTask.java
+++ core/src/main/java/net/praqma/clearcase/util/setup/BaselineTask.java
@@ -36,6 +36,7 @@ public class BaselineTask extends AbstractTask {
 			/* Components not given, skipping */
 		}
 		
+		System.out.println( "PUTTING BASELINE " + name );
 		context.baselines.put( name, Baseline.create( name, component, context.path, LabelBehaviour.valueOf( label ), identical.length() > 0, null, components ) );
 	}
 
