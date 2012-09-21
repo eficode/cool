08eb9630fd7f2fe4ae8082f821e3624da739f397
wolfgarnet <wolfgarnet@gmail.com>
2012-09-21 10:26:01 +0200

    Added linux support

--- core/src/main/java/net/praqma/clearcase/Cool.java
+++ core/src/main/java/net/praqma/clearcase/Cool.java
@@ -1,5 +1,6 @@
 package net.praqma.clearcase;
 
+import net.praqma.util.execute.CommandLine;
 import net.praqma.util.execute.CommandLineInterface.OperatingSystem;
 
 public abstract class Cool {
@@ -11,7 +12,6 @@ public abstract class Cool {
 	public static final String delim = "::";
 	
 	public static OperatingSystem getOS() {
-		return OperatingSystem.WINDOWS;
-		
+        return CommandLine.getInstance().getOS();
 	}
 }
