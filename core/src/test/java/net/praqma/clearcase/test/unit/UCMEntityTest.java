916f4961c39d5e3b0904cd7f9c13076e8fbb7f88
unknown <wolfgarnet@gmail.com>
2013-04-29 15:20:50 +0200

    [case 9017] Updated unit tests

--- core/src/test/java/net/praqma/clearcase/test/unit/UCMEntityTest.java
+++ core/src/test/java/net/praqma/clearcase/test/unit/UCMEntityTest.java
@@ -11,5 +11,24 @@ public class UCMEntityTest {
 	public void testInitialize() throws UnableToInitializeEntityException {
 		String c = "component:_System@\\ccucm-9-22339111_PVOB";
 		Component.get( c );
+
+        String c2 = "component:sødræv@\\ccucm-9-22339111_PVOB";
+        Component comp2 = Component.get( c2 );
+
+        String c3 = "component:_System@\\rød_PVOB";
+        Component.get( c3 );
 	}
+
+    @Test( expected = UnableToInitializeEntityException.class )
+    public void testInitializeFail() throws UnableToInitializeEntityException {
+        String c2 = "component:sødræv@@@@@\\ccucm-9-22339111_PVOB";
+        Component.get( c2 );
+    }
+
+    @Test( expected = UnableToInitializeEntityException.class )
+    public void testInitializeFail2() throws UnableToInitializeEntityException {
+        String c2 = "component:_System@\\coo l_PVOB";
+        Component comp2 = Component.get( c2 );
+        System.out.println( comp2 );
+    }
 }
