2850c3cc1161373e3bb13f2785d1b1f63179e75a
Christian Wolfgang <wolfgarnet@gmail.com>
2013-05-03 13:07:41 +0200

    [case 9179] Updated test

--- core/src/test/java/net/praqma/clearcase/command/listype/ListTypeFuncTest.java
+++ core/src/test/java/net/praqma/clearcase/command/listype/ListTypeFuncTest.java
@@ -1,7 +1,41 @@
 package net.praqma.clearcase.command.listype;
 
+import net.praqma.clearcase.Branch;
+import net.praqma.clearcase.Find;
+import net.praqma.clearcase.command.ListType;
+import net.praqma.clearcase.exceptions.CleartoolException;
+import net.praqma.clearcase.exceptions.UnableToInitializeEntityException;
+import net.praqma.clearcase.test.junit.ClearCaseRule;
+import net.praqma.clearcase.ucm.entities.Version;
+import net.praqma.util.test.junit.LoggingRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * @author cwolfgang
  */
 public class ListTypeFuncTest {
+
+    private static Logger logger = Logger.getLogger( ListTypeFuncTest.class.getName() );
+
+    @ClassRule
+    public static ClearCaseRule ccenv = new ClearCaseRule( "list-type", "setup.xml" );
+
+    @ClassRule
+    public static LoggingRule lrule = new LoggingRule( "net.praqma" );
+
+    @Test
+    public void test() throws UnableToInitializeEntityException, CleartoolException {
+
+        ListType ls = new ListType().setBranchType().setLocal();
+        List<Branch> branches = ls.list();
+
+        assertThat( branches.size(), is( 2 ) );
+    }
 }
