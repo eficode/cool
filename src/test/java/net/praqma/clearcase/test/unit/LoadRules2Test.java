2ee6c9d48a943c8c80e3e8b71cbf4b38efc8ca87
Mads Nielsen <man@praqma.net>
2014-05-20 12:40:13 +0200

    Test

---
+++ src/test/java/net/praqma/clearcase/test/unit/LoadRules2Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.praqma.clearcase.test.unit;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author Mads
+ */
+public class LoadRules2Test {
+    
+    @org.junit.Test
+    public void testLoadRules2() {
+        //TODO: Implement a test
+        assertTrue(true);
+    } 
+}
