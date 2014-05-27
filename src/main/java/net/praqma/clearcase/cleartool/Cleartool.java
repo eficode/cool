42d5f3884ccc9dae805023f9a4be9d7dc9c5c60b
Mads Nielsen <man@praqma.net>
2014-05-27 18:21:58 +0200

    Less verbos debug out

--- src/main/java/net/praqma/clearcase/cleartool/Cleartool.java
+++ src/main/java/net/praqma/clearcase/cleartool/Cleartool.java
@@ -63,6 +63,10 @@ public abstract class Cleartool extends Cool {
 				throw new NoLicensesException( "No licenses available", e );
 			} else if( e.getMessage().contains( "There are no valid licenses in the NT registry for ClearCase" )) {
 				throw new NoLicensesException( "No licenses available", e );
+            } else if (e.getMessage().contains( "No matching entries found for view tag" )) {
+                //This is an exception, when we fail to get view, it usually means we just create it afterwards
+                log.log(Level.WARNING, String.format( "Failed to execute command %s%nMessage is: %s", e.getCommand(),e.getMessage() ) );
+                throw e;
             } else {
                 log.log(Level.SEVERE, "Unspecified cleartool error", e);
 				throw e;
