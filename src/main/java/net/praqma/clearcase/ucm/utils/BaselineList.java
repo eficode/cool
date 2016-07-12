e58315f4e1c542c4dd847e95afe462398a3e923c
Thierry Lacour <thi@praqma.net>
2016-07-12 09:41:37 +0200

    Changed some unstable code to fix the functional tests

--- src/main/java/net/praqma/clearcase/ucm/utils/BaselineList.java
+++ src/main/java/net/praqma/clearcase/ucm/utils/BaselineList.java
@@ -219,9 +219,9 @@ public class BaselineList extends ArrayList<Baseline> {
         String cmd;
 
         if (SystemUtils.IS_OS_WINDOWS) {
-            cmd = "lsbl -fmt %Xn¤%Nd¤%[label_status]p\\n -component " + component + " -stream " + stream + (level != null ? " -level " + level.toString() : "");
+            cmd = "lsbl -fmt %Xn::%Nd::%[label_status]p\\n -component " + component + " -stream " + stream + (level != null ? " -level " + level.toString() : "");
         } else {
-            cmd = "lsbl -fmt %Xn¤%Nd¤%[label_status]p\\\\n -component " + component + " -stream " + stream + (level != null ? " -level " + level.toString() : "");
+            cmd = "lsbl -fmt %Xn::%Nd::%[label_status]p\\\\n -component " + component + " -stream " + stream + (level != null ? " -level " + level.toString() : "");
         }
         try {
             bls_str = Cleartool.run(cmd).stdoutList;
@@ -235,7 +235,7 @@ public class BaselineList extends ArrayList<Baseline> {
         List<Baseline> bls = new ArrayList<Baseline>();
 
         for (String bl : bls_str) {
-            String[] split = bl.split("¤");
+            String[] split = bl.split("::");
             Baseline baseline = Baseline.get(split[0]);
             try {
                 baseline.setDate(split[1]);
@@ -259,7 +259,7 @@ public class BaselineList extends ArrayList<Baseline> {
             if (bl1.getDate() == null) {
                 return -1;
             }
-            return (int) ((bl1.getDate().getTime() / 1000) - (bl2.getDate().getTime() / 1000));
+            return bl2.getDate().compareTo(bl1.getDate());
         }
     }
 
@@ -273,7 +273,7 @@ public class BaselineList extends ArrayList<Baseline> {
             if (bl1.getDate() == null) {
                 return 1;
             }
-            return (int) ((bl2.getDate().getTime() / 1000) - (bl1.getDate().getTime() / 1000));
+            return bl1.getDate().compareTo(bl2.getDate());
         }
     }
 }
--- src/main/java/net/praqma/clearcase/ucm/utils/filters/AfterBaseline.java
+++ src/main/java/net/praqma/clearcase/ucm/utils/filters/AfterBaseline.java
@@ -1,12 +1,12 @@
 package net.praqma.clearcase.ucm.utils.filters;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.logging.Logger;
-
 import net.praqma.clearcase.ucm.entities.Baseline;
 import net.praqma.clearcase.ucm.utils.BaselineFilter;
 import net.praqma.clearcase.ucm.utils.BaselineList;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.logging.Logger;
 
 public class AfterBaseline extends BaselineFilter {
 	
@@ -24,29 +24,24 @@ public class AfterBaseline extends BaselineFilter {
         list.ensureBaseline( after );
     }
 
-    @Override
-	public int filter( BaselineList baselines ) {
+	@Override
+	public int filter(BaselineList baselines) {
 		
 		/* Sort the baselines */
-		Collections.sort( baselines, new BaselineList.AscendingDateSort() );
-		
+		Collections.sort(baselines, new BaselineList.AscendingDateSort());
+
 		int pruned = 0;
 		Iterator<Baseline> itAfter = baselines.iterator();
-		while( itAfter.hasNext() ) {
+		while (itAfter.hasNext()) {
 			Baseline baseline = itAfter.next();
-			logger.fine( "Removing " + baseline.getNormalizedName() );
-			if( baseline.equals( after ) ) {
-				/* We found the baseline we were looking for */
-				/* Let's remove this too */
-				itAfter.remove();
-				pruned++;
+			logger.fine("Removing " + baseline.getNormalizedName());
+			itAfter.remove();
+			pruned++;
+			if (baseline.equals(after)) {
+				/* Stop pruning once we've found our baseline */
 				break;
-			} else {
-				itAfter.remove();
-				pruned++;
 			}
 		}
-		
 		return pruned;
 	}
 
